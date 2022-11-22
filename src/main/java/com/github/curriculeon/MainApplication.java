52a0ec3f4bb3565669392a03e57f6759765e226b
Leon Hunter <xleonhunter@gmail.com>
2022-11-21 19:40:15 -0500

    updated with list demos

--- src/main/java/com/github/curriculeon/MainApplication.java
+++ src/main/java/com/github/curriculeon/MainApplication.java
@@ -1,11 +1,97 @@
 package com.github.curriculeon;
 
-import java.util.GregorianCalendar;
+import java.util.*;
 
 public class MainApplication {
-    public static void main(String[] args) {
-        
+    public static void removingFromList() {
+        PersonDefault person4 = new PersonDefault(29, "Leon", "Hunter");
+        PersonDefault person5 = new PersonDefault(28, "Leon", "Hunter");
+        PersonDefault person6 = new PersonDefault(27, "Leon", "Hunter");
+        List<PersonDefault> personList = new LinkedList<>();
+        personList.add(person4);
+        personList.add(person5);
+        personList.add(person6);
+        personList.add(new PersonDefault(29, null, null));
+        personList.remove(3);
+
+        printList(personList);
     }
+
+    public static void reverseAndPrintList(List<PersonDefault> personDefaults) {
+        Collections.reverse(personDefaults);
+        printList(personDefaults);
+    }
+
+    public static void printList(List<PersonDefault> personDefaults) {
+        for(PersonDefault personDefault : personDefaults) {
+            System.out.println(personDefault);
+        }
+    }
+
+    public static void addingToList() {
+        PersonDefault person4 = new PersonDefault(29, "Leon", "Hunter");
+        PersonDefault person5 = new PersonDefault(28, "Leon", "Hunter");
+        PersonDefault person6 = new PersonDefault(27, "Leon", "Hunter");
+        List<PersonDefault> personList = new ArrayList<>();
+        personList.add(person4);
+        personList.add(person5);
+        personList.add(person6);
+        personList.add(new PersonDefault(29, null, null));
+
+    }
+
+
+    public static void removingFromArray() {
+        PersonDefault person4 = new PersonDefault(29, "Leon", "Hunter");
+        PersonDefault person5 = new PersonDefault(28, "Leon", "Hunter");
+        PersonDefault person6 = new PersonDefault(27, "Leon", "Hunter");
+
+        PersonDefault[] defaultPeople = new PersonDefault[]{
+                person4, person5, person6
+        };
+
+        defaultPeople[2] = null;
+
+        defaultPeople = new PersonDefault[]{
+                person4, person5, null
+        };
+
+        PersonDefault[] defaultPersonPlusOneMore = new PersonDefault[defaultPeople.length-1];
+        for (int i = 0; i < defaultPersonPlusOneMore.length-1; i++) {
+            defaultPersonPlusOneMore[i] = defaultPeople[i];
+        }
+    }
+
+
+    public static void addingToArray() {
+        PersonDefault person4 = new PersonDefault(29, "Leon", "Hunter");
+        PersonDefault person5 = new PersonDefault(28, "Leon", "Hunter");
+        PersonDefault person6 = new PersonDefault(27, "Leon", "Hunter");
+
+        PersonDefault[] defaultPeople = new PersonDefault[]{
+                person4, person5, person6
+        };
+
+        PersonDefault[] defaultPersonPlusOneMore = new PersonDefault[defaultPeople.length+1];
+        for (int i = 0; i < defaultPersonPlusOneMore.length; i++) {
+            defaultPersonPlusOneMore[i] = defaultPeople[i];
+        }
+        defaultPersonPlusOneMore[3] = new PersonDefault(0, null, null);
+    }
+
+    public static void doSomethingToPeopleList(List<PersonDefault> list) {
+        list.add(new PersonDefault(5, null, null));
+    }
+
+    public static void doSomethingToPeopleArray(PersonDefault[] array) {
+        PersonDefault[] defaultPersonPlusOneMore = new PersonDefault[array.length+1];
+        for (int i = 0; i < defaultPersonPlusOneMore.length; i++) {
+            defaultPersonPlusOneMore[i] = array[i];
+        }
+        defaultPersonPlusOneMore[array.length+1] = new PersonDefault(1, null, null);
+    }
+
+
     public static void demo1() {
         GregorianCalendar birthdate1 = new GregorianCalendar(1993, 4, 23);
         GregorianCalendar birthdate2 = new GregorianCalendar(1994, 4, 23);
